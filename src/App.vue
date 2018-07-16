352ad12c6b6383ed94e46f20430e8777b3f8be7c
Michael Smith <smithms91@gmail.com>
2018-07-16 01:17:04 -0400

    First commit to github of VueJS tile website

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <app-header></app-header>
+  </div>
+</template>
+
+<script>
+import Header from './Header'
+
+export default {
+  data() {
+    return {
+    }
+  },
+  components: {
+    appHeader: Header
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  border: none;
+  outline: none;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/Header.vue
@@ -0,0 +1,72 @@
+<template>
+  <nav class="navbar">
+    
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+  methods: {
+    openNav() {
+      let body = document.getElementById('app');
+      body.style.transform = "translateX(50px)";
+    }
+  }
+}
+</script>
+
+<style scoped>
+  #header-container {
+    display: flex;
+    width: 100%;
+    height: 11%;
+    align-items: center;
+    justify-content: space-between;
+    background-color: red;
+    padding: 50px;
+    position: absolute;
+  }
+
+  .logo {
+    float: left;
+  }
+
+  .logo h1 {
+    font-size: 3em;
+  }
+
+  .logo p {
+    font-size: 2em;
+    letter-spacing: 6px;
+  }
+
+  .sandwich-container {
+    float: right;
+  }
+
+  .line {
+    width: 50px;
+    height: 5px;
+    background: #000;
+    margin-top: 12px;
+  }
+
+
+  /* Tablet */
+  @media only screen and (min-width: 600px) {
+    
+  }
+
+
+  /* Desktop */
+  @media only screen and (min-width: 1000px) {
+    .sandwich-container {
+      display: none;
+    }
+  }
+</style>
+
